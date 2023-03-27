bd8173178b2989c2c4c363b94073276283637c79
Mateus Alves <mateusalves5942@gmail.com>
2023-03-27 16:14:24 -0300

    EndPoint getCursos finalizado

--- Back-End/index.js
+++ Back-End/index.js
@@ -4,3 +4,37 @@
  * Autor: Mateus Alves da Silva
  * Versão: 1.0
  *********************/
+const express = require('express')
+const cors =require('cors')
+const bodyParer = require('body-parser')
+
+const app = express()
+
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*')
+
+    //Permite definir quais métodos poderão ser utilizados nas requisições da API
+    response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+
+    app.use(cors())
+
+    next()
+})
+
+const funcoes = require('./modulo/functions.js')
+
+app.get('/v1/lion-school/cursos', cors(), async function(request, response, next){
+
+    let cursos = funcoes.getCursos()
+
+    if(cursos){
+        response.status(200)
+        response.json(cursos)
+    } else{
+        response.status(500)
+    }
+})
+
+app.listen(8080, function(){
+    console.log('servidor aguardando requisições na porta 8080.')
+})
